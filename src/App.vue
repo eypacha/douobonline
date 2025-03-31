5af25f1b3a8a8c7c22abb7c3233c74e28fa0d813
Ey Pacha! <pachaguionbajo@gmail.com>
2025-03-31 12:42:38 -0300

    initialize Vue 3 project with Vite, Tailwind CSS, and essential assets

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <DouobLogo/>
+    <div>
+      <p class="mx-auto text-base text-center text-[transparent]">douob is a system made by <a href="http://verne.studio">verne.studio</a> that allows us to connect and take control of the screens that surround us, shop windows, events, cinemas, exhibitions, museums and public roads. contact us for more information</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import DouobLogo from './components/DouobLogo.vue';
+
+</script>
+
+<style scoped lang="scss">
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  max-width: 600px;
+}
+</style>
